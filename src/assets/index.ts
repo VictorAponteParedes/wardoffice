f8c476560e9eb99b1d70b160cb31609530658b68
Victor Aponte <vaponte520@gmail.com>
2025-11-08 15:43:06 -0300

    chore(assets): update image import and clean up export syntax
    Replace jfif image with jpeg version and remove unnecessary whitespace

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,4 @@
 import sudBackground from "./images/temploFondo2.jfif";
-import sudLogo from "./images/temploFondo.jfif";
+import sudLogo from "./images/temploFondo.jpeg";
 
-
-
-export {
-    sudBackground,
-    sudLogo,
-}
+export { sudBackground, sudLogo };
